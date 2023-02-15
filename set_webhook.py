2e2e7fccd9824876c99c54c15293f6e2cd8d15dc
djumanov <diyorbekjumanov1120@gmail.com>
2023-02-15 16:44:58 +0500

    init

---
+++ set_webhook.py
@@ -0,0 +1,8 @@
+from telegram import Bot
+import os
+
+TOKEN = os.environ['TOKEN']
+bot = Bot(token=TOKEN)
+
+bot.set_webhook(url='https://codeschoolapp.pythonanywhere.com/')
+print(bot.get_webhook_info())
