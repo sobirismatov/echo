91a01c888ed52c1713126a3e9f42d85407da4538
djumanov <diyorbekjumanov1120@gmail.com>
2023-02-15 16:49:59 +0500

    init

--- main.py
+++ main.py
@@ -2,10 +2,15 @@ from flask import Flask, request
 
 echo_app = Flask(__name__)
 
-@echo_app.route('/')
+@echo_app.route('/', methods=['POST', 'GET'])
 def main():
-    return 'ok'
+    if request.method == 'GET':
+        return 'hi from Husniddin'
+    elif request.method == 'POST':
+        data = request.get_json(force=True)
+        print(data)
 
+        return 'hello'
 
 if __name__ == '__main__':
     echo_app.run()
