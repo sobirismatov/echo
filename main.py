27b6ef9fe8bf25a96d9ffdf7b0b5451f6a5390d0
sobirismatov <name@uzcoder96@gmail.com>
2023-02-15 17:57:46 +0500

    echo

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ echo_app = Flask(__name__)
 @echo_app.route('/', methods=['POST', 'GET'])
 def main():
     if request.method == 'GET':
+        print("dsgd")
         return 'hi from Husniddin'
+        
     elif request.method == 'POST':
         data = request.get_json(force=True)
         chat_id=data["massage"]["from"]["id"]
